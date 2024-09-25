009187d83c6aab5f82b9347ef728548ca12193f6
ferbautistamollo <ferbautistamollo@gmail.com>
2024-09-25 18:38:58 -0400

    Creación de servicio para documentos del afiliado

---
+++ src/procedure-documents/procedure-documents.service.ts
@@ -0,0 +1,30 @@
+import { Injectable, Logger, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { ProcedureDocument } from './entities/procedure-document.entity';
+
+@Injectable()
+export class ProcedureDocumentsService {
+  private readonly logger = new Logger('ProcedureDocumentsService');
+  constructor(
+    @InjectRepository(ProcedureDocument)
+    private readonly procedureDocumentsRepository: Repository<ProcedureDocument>,
+  ) {}
+  //Mostrar todos los documentos, nombre y ultima actualización
+  async findAll(): Promise<Partial<ProcedureDocument>[]> {
+    return this.procedureDocumentsRepository.find({
+      select: ['name', 'updated_at'],
+    });
+  }
+
+  async findOne(id: number): Promise<ProcedureDocument> {
+    const procedureDocument = await this.procedureDocumentsRepository.findOneBy(
+      { id },
+    );
+
+    if (!procedureDocument)
+      throw new NotFoundException(`Documento with: ${id} not found`);
+
+    return procedureDocument;
+  }
+}
