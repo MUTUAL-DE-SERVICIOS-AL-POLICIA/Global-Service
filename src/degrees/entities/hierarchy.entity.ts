e83e5bd406036634a683807c5b908f1c0f7d4ae2
ferbautistamollo <ferbautistamollo@gmail.com>
2024-10-14 02:05:01 -0400

    creacion de entidad jerarquia para la obtencion de datos

--- src/degrees/entities/hierarchy.entity.ts
+++ src/degrees/entities/hierarchy.entity.ts
@@ -1,1 +1,17 @@
-export class Hierarchy {}
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Degree } from './';
+
+@Entity({ schema: 'public', name: 'hierarchies', synchronize: false })
+export class Hierarchy {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  code: string;
+
+  @Column()
+  name: string;
+
+  @OneToMany(() => Degree, (degrees) => degrees.hierarchy)
+  degrees: Degree[];
+}
