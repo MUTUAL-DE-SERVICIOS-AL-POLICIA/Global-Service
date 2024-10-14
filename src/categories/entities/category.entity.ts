ec8051ea72d2ce3f0ccb592bb966bcc9c9242fef
ferbautistamollo <ferbautistamollo@gmail.com>
2024-10-14 12:14:39 -0400

    Injecion de datos de la tabla para manipular datos

--- src/categories/entities/category.entity.ts
+++ src/categories/entities/category.entity.ts
@@ -1,1 +1,19 @@
-export class Category {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ schema: 'public', name: 'categories', synchronize: false })
+export class Category {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  from: number;
+
+  @Column()
+  to: number;
+
+  @Column()
+  name: string;
+
+  @Column({ precision: 13, scale: 2 })
+  percentage: number;
+}
