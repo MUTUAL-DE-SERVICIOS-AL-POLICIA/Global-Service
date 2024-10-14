c792f96f4777a25a7cae82ca7f4c7fd4f7823eb7
ferbautistamollo <ferbautistamollo@gmail.com>
2024-10-14 02:04:31 -0400

    creacion de entidad grados para la obtencion de datos

--- src/degrees/entities/degree.entity.ts
+++ src/degrees/entities/degree.entity.ts
@@ -1,1 +1,26 @@
-export class Degree {}
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Hierarchy } from './';
+
+@Entity({ schema: 'public', name: 'degrees', synchronize: false })
+export class Degree {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  code: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  shortened: string;
+
+  @Column()
+  correlative?: number;
+
+  @Column()
+  is_active: boolean;
+
+  @ManyToOne(() => Hierarchy, (hierarchy) => hierarchy.degrees)
+  hierarchy: Hierarchy;
+}
