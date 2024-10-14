ff0760aad4c6a4667a00aa1f1647d20ab1efd2a0
ferbautistamollo <ferbautistamollo@gmail.com>
2024-10-14 12:12:59 -0400

    Logica de End Points para sacar uno o todas las categorias

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,13 +1,24 @@
-import { Injectable } from '@nestjs/common';
-
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Category } from './entities/category.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 @Injectable()
 export class CategoriesService {
+  constructor(
+    @InjectRepository(Category)
+    private readonly categoriesRepository: Repository<Category>,
+  ) {}
 
-  findAll() {
-    return `This action returns all categories`;
+  async findAllCategories(): Promise<Partial<Category>[]> {
+    return this.categoriesRepository.find({
+      select: ['id', 'name'],
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} category`;
+  async findOneCategory(id: number): Promise<Category> {
+    const category = await this.categoriesRepository.findOneBy({ id });
+
+    if (!category) throw new NotFoundException(`category with ${id} not found`);
+    return category;
   }
 }
