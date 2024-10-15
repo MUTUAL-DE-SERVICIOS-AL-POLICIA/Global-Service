7e09be00184cfd7df1fb12e926453d73c62765f9
ferbautistamollo <ferbautistamollo@gmail.com>
2024-10-15 09:42:51 -0400

    Creación de la entidad, valores para la obtencion de registro de la tabla cities

--- src/cities/entities/city.entity.ts
+++ src/cities/entities/city.entity.ts
@@ -1,1 +1,40 @@
-export class City {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ schema: 'public', name: 'cities', synchronize: false })
+export class City {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  first_shortened: string;
+
+  @Column()
+  second_shortened: string;
+
+  @Column()
+  third_shortened: string;
+
+  @Column()
+  to_bank?: string;
+
+  @Column({ type: 'numeric', precision: 10, scale: 2 })
+  latitude?: number;
+
+  @Column()
+  longitude?: number;
+
+  @Column()
+  company_address: string;
+
+  @Column()
+  phone_prefix: number;
+
+  @Column('json')
+  company_phones: any;
+
+  @Column('json')
+  company_cellphones: any;
+}
