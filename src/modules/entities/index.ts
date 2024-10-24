9e0ef55e5725356ff7dfec55c7f310593b70f5f1
ferbautistamollo <ferbautistamollo@gmail.com>
2024-10-24 17:01:13 -0400

    Creacion de arichivo de barril para modules e importacion

---
+++ src/modules/entities/index.ts
@@ -0,0 +1,4 @@
+export { ProcedureModality } from './procedure_modality.entity';
+export { ProcedureRequirement } from './procedure_requirement.entity';
+export { ProcedureType } from './procedure_type.entity';
+export { Module } from './module.entity';
