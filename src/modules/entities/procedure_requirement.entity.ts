f93f5b5984db86e19492012bc44828fd2d73cda8
ferbautistamollo <ferbautistamollo@gmail.com>
2024-10-25 10:26:14 -0400

    Creación de entidad de documentos requeridos por modalidad y ajuste de relaciones

--- src/modules/entities/procedure_requirement.entity.ts
+++ src/modules/entities/procedure_requirement.entity.ts
@@ -1,3 +1,44 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { ProcedureModality } from './';
+import { ProcedureDocument } from 'src/procedure-documents/entities/procedure-document.entity';
+@Entity({
+  schema: 'public',
+  name: 'procedure_requirements',
+  synchronize: false,
+})
 export class ProcedureRequirement {
-  
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ManyToOne(
+    () => ProcedureModality,
+    (procedureModality) => procedureModality.procedureRequirements,
+  )
+  procedureModality: ProcedureModality;
+
+  @ManyToOne(
+    () => ProcedureDocument,
+    (procedureDocument) => procedureDocument.procedureRequirements,
+  )
+  procedureDocument: ProcedureDocument;
+
+  @Column()
+  number: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @DeleteDateColumn()
+  deletedAt: Date;
 }
