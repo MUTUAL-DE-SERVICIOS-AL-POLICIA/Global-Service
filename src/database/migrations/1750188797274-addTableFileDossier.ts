bb949624c269421e2676a860fe1e0342e5713710
ferbautistamollo <ferbautistamollo@gmail.com>
2025-07-14 14:02:37 -0400

    LINTEANDO MIGRACION DE CREACIO NDE TABLA DE EXPEDIENTES

--- src/database/migrations/1750188797274-addTableFileDossier.ts
+++ src/database/migrations/1750188797274-addTableFileDossier.ts
@@ -1,42 +1,44 @@
-import { MigrationInterface, QueryRunner, Table } from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export class AddTableFileDossier1750188797274 implements MigrationInterface {
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.createTable(new Table({
-            name: 'file_dossiers',
-            columns: [
-                {
-                    name: 'id',
-                    type: 'bigserial',
-                    isPrimary: true,
-                },
-                {
-                    name: 'name',
-                    type: 'varchar',
-                    isNullable: false,
-                },
-                {
-                    name: 'shortened',
-                    type: 'varchar',
-                    isNullable: false,
-                },
-                {
-                    name: 'created_at',
-                    type: 'timestamp',
-                    default: 'CURRENT_TIMESTAMP',
-                },
-                {
-                    name: 'updated_at',
-                    type: 'timestamp',
-                    default: 'CURRENT_TIMESTAMP',
-                },
-                {
-                    name: 'deleted_at',
-                    type: 'timestamp',
-                    isNullable: true,
-                }
-            ],
-    }));
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'file_dossiers',
+        columns: [
+          {
+            name: 'id',
+            type: 'bigserial',
+            isPrimary: true,
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'shortened',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP',
+          },
+          {
+            name: 'deleted_at',
+            type: 'timestamp',
+            isNullable: true,
+          },
+        ],
+      }),
+    );
 
     await queryRunner.query(`
         INSERT INTO global.file_dossiers (name, shortened)
@@ -44,9 +46,9 @@ export class AddTableFileDossier1750188797274 implements MigrationInterface {
         ('Fondo de Retiro', 'FR'),
         ('Complemento Económico', 'CE');
     `);
-    }
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.dropTable('file_dossiers');
-    }
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('file_dossiers');
+  }
 }
